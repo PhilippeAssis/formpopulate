f8d577c4eb130a6153727ba3f5d1d45cda3962de
Philippe Assis <assis@philippeassis.com>
2016-04-11 00:15:31 -0300

    bower

--- jquery.formpopulate.js
+++ jquery.formpopulate.js
@@ -2,45 +2,44 @@
  * Form Populate - Create by Philippe Assis
  * www.philippeassis.com
  */
-(function ($) {
-    $.fn.formPopulate = function (data) {
-        var inputs = $(this).find('select,input,textarea');
 
-        if (inputs.length > 0) {
-            for (var i = 0; inputs.length >= i; i++) {
-                var input = $(inputs[i]);
-                var name = input.attr('name');
+$.fn.formPopulate = function (data) {
+    var inputs = $(this).find('select,input,textarea');
+
+    if (inputs.length > 0) {
+        for (var i = 0; inputs.length >= i; i++) {
+            var input = $(inputs[i]);
+            var name = input.attr('name');
 
 
-                if (data[name] == undefined) {
-                    continue;
+            if (data[name] == undefined) {
+                continue;
+            }
+            else {
+                var dataValue = data[name];
+
+                if (input.is('input')) {
+                    switch (input.attr('type')) {
+                        case 'checkbox':
+                            input.prop('checked', dataValue);
+                            break;
+                        case 'radio':
+                            input.prop('checked', (dataValue == input.attr('value') ? true : false));
+                            break;
+                        case 'text':
+                            input.val(dataValue);
+                            break;
+                    }
                 }
-                else {
-                    var dataValue = data[name];
-
-                    if (input.is('input')) {
-                        switch (input.attr('type')) {
-                            case 'checkbox':
-                                input.prop('checked', dataValue);
-                                break;
-                            case 'radio':
-                                input.prop('checked', (dataValue == input.attr('value') ? true : false));
-                                break;
-                            case 'text':
-                                input.val(dataValue);
-                                break;
-                        }
-                    }
-                    else if (input.is('select')) {
-                        $("option", input).each(function () {
-                            $(this).attr('selected', ($(this).val() === dataValue));
-                        })
-                    }
-                    else if (input.is('textarea')) {
-                        input.val(dataValue);
-                    }
+                else if (input.is('select')) {
+                    $("option", input).each(function () {
+                        $(this).attr('selected', ($(this).val() === dataValue));
+                    })
+                }
+                else if (input.is('textarea')) {
+                    input.val(dataValue);
                 }
             }
         }
     }
-})
+}
